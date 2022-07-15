c26cf731315f232e3d88abfd8d66806ddc215433
IskanMr <zahraulathiyah@gmail.com>
2022-07-15 17:36:36 +0700

    feat(faq): finishing faq page + add pageTitle size for mobile

--- src/components/QNA/Question.jsx
+++ src/components/QNA/Question.jsx
@@ -2,17 +2,16 @@ import { Disclosure } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/solid';
 
 export function Question({ item }) {
-    const disclosureContainerCSS =
-        'my-6  bg-gradient-to-r from-cstmglass2 to-cstmglass rounded-lg';
+    const disclosureContainerCSS = 'my-2 bg-white rounded-lg items-center';
     const disclosureButtonCSSClose =
-        'flex justify-between w-full px-6 py-4 text-sm md:text-[18px] font-gothammedium text-left focus:outline-none ';
+        'flex justify-between items-center w-full p-3 md:px-6 md:py-4 text-md md:text-[18px] font-gothammedium text-left focus:outline-none ';
     const disclosureButtonCSSOpen =
-        'flex justify-between w-full px-6 py-4 text-sm md:text-[18px] font-gothammedium text-left focus:outline-none shadow-md';
+        'flex justify-between items-center w-full p-3 md:px-6 md:py-4 text-md md:text-[18px] font-gothammedium text-left focus:outline-none shadow-md';
 
     const disclosurePanelCSS =
-        'px-6 py-4 pb-2 text-sm md:text-[14px] font-gothambook';
+        'p-3 md:px-6 md:py-4 pb-2 items-center text-sm md:text-[14px] font-gothambook';
     const openPanelChevronIconCSS = 'transform rotate-180';
-    const ChevronIconCSS = 'w-5 h-5 text-cstmdarkergreen';
+    const ChevronIconCSS = 'w-5 h-5 md:w-7 md:h-7 text-cstmdarkergreen';
 
     return (
         <>
@@ -26,11 +25,13 @@ export function Question({ item }) {
                                     : disclosureButtonCSSClose
                             }
                         >
-                            <div className='place-content-center'>
-                                <span className='text-[24px] mr-3 text-oranges-default'>
+                            <div className='flex items-center'>
+                                <span className='text-xl md:text-2xl mr-3 text-oranges-default'>
                                     Q
                                 </span>
-                                <span>{item.question}</span>
+                                <span className='flex-grow'>
+                                    {item.question}
+                                </span>
                             </div>
 
                             <ChevronDownIcon
@@ -44,8 +45,8 @@ export function Question({ item }) {
                                 static
                                 className={disclosurePanelCSS}
                             >
-                                <div className='flex'>
-                                    <span className='text-[24px] mr-3 text-main-blue'>
+                                <div className='flex items-center'>
+                                    <span className=' text-xl md:text-2xl mr-3 text-main-blue'>
                                         A
                                     </span>
                                     <span className='flex-grow'>
--- src/pages/qna/index.jsx
+++ src/pages/qna/index.jsx
@@ -6,6 +6,7 @@ import { Question } from '../../components/QNA/Question';
 import Questions from '../../resources/questions.json';
 
 import { ReactComponent as Search } from '../../assets/icons/search.svg';
+import background from '../../assets/images/pattern-light.png';
 
 export function QNA() {
     const [list, setList] = useState(Questions);
@@ -15,39 +16,44 @@ export function QNA() {
     return (
         <>
             <HtmlHead title='Pertanyaan Umum' decription='Yes' />
-            <GenericPage>
-                <PageTitle title='Pertanyaan Umum' />
+            <div
+                className='bg-white_bone'
+                style={{ backgroundImage: `url(${background})` }}
+            >
+                <GenericPage>
+                    <PageTitle title='Pertanyaan Umum' />
 
-                <div className='flex w-100 mt-6  justify-items-center place-content-center'>
-                    <label className='flex  w-[100%] md:w-[642px] py-2 px-3 bg-white rounded-xl items-center '>
-                        <span>
-                            <Search className='h-[14px]' />
-                        </span>
-                        <input
-                            className='flex flex-grow ml-2 border-none outline-none'
-                            placeholder='Ketik pertanyaanmu...'
-                            type='text'
-                            onChange={(e) => {
-                                onInputChange(e.target.value);
-                            }}
-                        />
-                    </label>
-                </div>
-                <div className='flex justify-center'>
-                    <div className='flex flex-col w-100 md:w-3/4'>
-                        {list.map((item) => (
-                            <Question item={item} />
-                        ))}
+                    <div className='flex w-100 mt-6  justify-items-center place-content-center'>
+                        <label className='flex  w-[100%] md:w-[642px] p-3 bg-white rounded-xl items-center text-sm md:text-base'>
+                            <span>
+                                <Search className='h-4 md:h-5' />
+                            </span>
+                            <input
+                                className='flex flex-grow ml-1 md:ml-2 border-none outline-none'
+                                placeholder='Ketik pertanyaanmu...'
+                                type='text'
+                                onChange={(e) => {
+                                    onInputChange(e.target.value);
+                                }}
+                            />
+                        </label>
                     </div>
-                </div>
-                <div className='flex justify-center'>
-                    Pertanyaan kamu tidak ada di laman ini? Silahkan hubungi
-                    <a href='/pertanyaan-umum' className='ml-1'>
-                        {' '}
-                        sini
-                    </a>
-                </div>
-            </GenericPage>
+                    <div className='flex justify-center mt-6'>
+                        <div className='flex flex-col w-100 md:w-3/4'>
+                            {list.map((item) => (
+                                <Question item={item} />
+                            ))}
+                        </div>
+                    </div>
+                    <div className='flex justify-center mt-2 md:mt-4'>
+                        Pertanyaan kamu tidak ada di laman ini? Silahkan hubungi
+                        <a href='/pertanyaan-umum' className='ml-1'>
+                            {' '}
+                            sini
+                        </a>
+                    </div>
+                </GenericPage>
+            </div>
         </>
     );
 }
