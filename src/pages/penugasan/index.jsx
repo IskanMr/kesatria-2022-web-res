a481b3bead4f4558177308cfcc7715d2785a01df
bintang <bintangrestu.yk01@gmail.com>
2022-07-17 20:22:32 +0700

    nunggu divisi materi

--- src/pages/penugasan/index.jsx
+++ src/pages/penugasan/index.jsx
@@ -4,68 +4,29 @@ import { GenericPage } from '../../components/GenericPage';
 import { HtmlHead } from '../../components/HtmlHead';
 import { PageTitle } from '../../components/PageTitle';
 import '../../styles/materi.scss';
+import image from '../../assets/pattern1_story 1.svg';
 
 export function Penugasan() {
     return (
-        <>
+        <div
+            style={{ backgroundImage: `url(${image})` }}
+            className='w-full h-screen bg-repeat-x bg-cover'
+        >
             <HtmlHead title='Penugasan' decription='Yes' />
             <PageTitle title='Penugasan' />
-            <section className='home' id='home'>
-                <div className='home-wrapper'>
-                    <div className='content-left'>
-                        <h1 className='heading'>Materi 2 : Judul Materi</h1>
-                        <p className='explanation'>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Etiam vel tempus nunc, sit amet sodales dui.
-                            Duis pellentesque elit ut magna tristique, id
-                            blandit augue venenatis. Aenean hendrerit neque eget
-                            rhoncus vehicula. Pellentesque habitant morbi
-                            tristique senectus et netus et malesuada fames ac
-                            turpis egestas. Phasellus ut ultricies ante. Proin
-                            ultrices pellentesque nulla, ut fringilla dolor
-                            mollis tristique. Cras non tincidunt enim, et rutrum
-                            arcu.
-                        </p>
-                    </div>
-                    <div className='round-box'>
-                        <Router>
-                            <button
-                                type='submit'
-                                id='materi-1'
-                                className='item-list1 block1'
-                            >
-                                {' '}
-                                Materi 1{' '}
-                            </button>
-                            <button
-                                type='submit'
-                                id='materi-2'
-                                className='item-list2 block2'
-                            >
-                                {' '}
-                                Materi 2{' '}
-                            </button>
-                            <button
-                                type='submit'
-                                id='materi-3'
-                                className='item-list3 block3'
-                            >
-                                {' '}
-                                Materi 3{' '}
-                            </button>
-                        </Router>
-                    </div>
-                    <div className='container2-box2'>
-                        <button
-                            type='submit'
-                            id='classroom'
-                            className='item-list4 block4'
-                        >
-                            GO TO CLASSROOM
-                        </button>
-                    </div>
+            <div className='flex -mt-8'>
+                <div className='relative flex w-full h-full shadow hover:shadow-3xl rounded-xl place-content-center pt-8 shadow-lg shadow-blue-500/50'>
+                    <iframe
+                        src='https://drive.google.com/file/d/1canFad3kwDTDxoo2FdayU39Hww2W5_k0/preview'
+                        style={{ width: 1800, height: 1000 }}
+                        allow='autoplay'
+                        title='Detail Penugasan'
+                        className='absolute w-full shadow hover:shadow-3xl shadow-blue-500/50 rounded-2xl place-content-center mt-8 '
+                    >
+                        documnet
+                    </iframe>
                 </div>
-            </section>
-        </>
+            </div>
+        </div>
     );
 }
