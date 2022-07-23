400af1af16c521b480e860ec6470aa6344ece41f
Iskan <iskan.mustamir@mail.ugm.ac.id>
2022-07-23 16:23:53 +0700

    feat: add countdown component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,12 @@
 import { HtmlHead } from '../components/HtmlHead';
+import { CountDown } from '../components/CountDown';
 
 export function Home() {
     return (
         <>
             <HtmlHead title='Beranda' decription='Yes' />
             <div className='flex flex-col bg-gray-lighter -mb-4'>
-                This is home
+                <CountDown />
             </div>
         </>
     );
