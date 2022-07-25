ee2c1c5344a9aa966bc21c4842e63f851330094a
Mufidus Sani <sanimufidus@gmail.com>
2022-07-25 22:56:22 +0700

    fix(jelajah-teknik): pages content

--- src/pages/jalan-teknik/index.jsx
+++ src/pages/jalan-teknik/index.jsx
@@ -16,34 +16,18 @@ export function JelajahTeknik() {
     const Children = [
         <>
             <div className='card-one w-[45vw] 768px:w-[65vw] flex-col flex justify-center px-[3rem] 768px:px-[1rem]'>
-                <div className='w-[45vmin] h-[25vmin] z-3 block absolute right-[-15vw] top-[-8vw] lg:top-[-5vw] lg:right-[-11vw] '>
+                {/* <div className='w-[45vmin] h-[25vmin] z-3 block absolute right-[-15vw] top-[-8vw] lg:top-[-5vw] lg:right-[-11vw] '>
                     <img
-                        className='scale-[70%] lg:scale-[78%]'
+                        className='w-100 h-100 z-[3] flex absolute scale-[70%] lg:scale-[78%]'
                         src={cloud}
                         alt=''
-                        style={{
-                            width: '100%',
-                            height: '100%',
-                            zIndex: '3',
-                            display: 'flex',
-                            position: 'absolute',
-                        }}
                     />
-                </div>
+                </div> */}
                 <h3 className='font-gill font-bold text-white text-center text-[4vmin] 1024px:text-[3.5vmin] 768px:text-[6.5vmin] my-[1.5rem]'>
                     NGULIK TEKNIK
                 </h3>
-                <div className='md:hidden'>
+                <div className='items-center justify-center flex h-100 w-100 ml-auto mr-auto md:hidden'>
                     <img
-                        style={{
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            display: 'flex',
-                            height: '100%',
-                            width: '100%',
-                            marginLeft: 'auto',
-                            marginRight: 'auto',
-                        }}
                         src='https://media-exp1.licdn.com/dms/image/C5603AQEDk8-4kGrnZw/profile-displayphoto-shrink_200_200/0/1630935435708?e=1660176000&v=beta&t=PNfAzjZMoa-mMuT_yxhGUAOsVXCjjeFlwio9D2s-3t4'
                         alt=''
                     />
@@ -66,20 +50,13 @@ export function JelajahTeknik() {
                         </button>
                     </div>
                 </div>
-                <div className='w-[45vmin] h-[25vmin] z-3 block absolute left-[-15vw] bottom-[-7vw] lg:bottom-[-5vw] lg:left-[-12vw] '>
+                {/* <div className='w-[45vmin] h-[25vmin] z-3 block absolute left-[-15vw] bottom-[-7vw] lg:bottom-[-5vw] lg:left-[-12vw] '>
                     <img
-                        className='scale-[70%] lg:scale-[78%]'
+                        className='w-100 h-100 z-[3] flex absolute scale-[70%] lg:scale-[78%]'
                         src={cloud}
                         alt=''
-                        style={{
-                            width: '100%',
-                            height: '100%',
-                            zIndex: '3',
-                            display: 'flex',
-                            position: 'absolute',
-                        }}
                     />
-                </div>
+                </div> */}
             </div>
             <div className='card-two w-[35vw] flex-row items-center justify-center hidden pr-[2rem] md:flex'>
                 <div className='h-[55vmin] w-[55vmin]'>
@@ -93,7 +70,7 @@ export function JelajahTeknik() {
         </>,
         <>
             <div className='card-one w-[45vw] 768px:w-[65vw] flex-col flex justify-center px-[3rem] 768px:px-[1rem]'>
-                <div className='w-[45vmin] h-[25vmin] z-3 block absolute right-[-15vw] top-[-8vw] lg:top-[-5vw] lg:right-[-11vw] '>
+                {/* <div className='w-[45vmin] h-[25vmin] z-3 block absolute right-[-15vw] top-[-8vw] lg:top-[-5vw] lg:right-[-11vw] '>
                     <img
                         className='scale-[70%] lg:scale-[78%]'
                         src={cloud}
@@ -106,7 +83,7 @@ export function JelajahTeknik() {
                             position: 'absolute',
                         }}
                     />
-                </div>
+                </div> */}
                 <h3 className='font-gill font-bold text-white text-center text-[4vmin] 1024px:text-[3.5vmin] 768px:text-[6.5vmin] my-[1.5rem]'>
                     JALAN-JALAN TEKNIK
                 </h3>
@@ -143,7 +120,7 @@ export function JelajahTeknik() {
                         </button>
                     </div>
                 </div>
-                <div className='w-[45vmin] h-[25vmin] z-3 block absolute left-[-15vw] bottom-[-7vw] lg:bottom-[-5vw] lg:left-[-12vw] '>
+                {/* <div className='w-[45vmin] h-[25vmin] z-3 block absolute left-[-15vw] bottom-[-7vw] lg:bottom-[-5vw] lg:left-[-12vw] '>
                     <img
                         className='scale-[70%] lg:scale-[78%]'
                         src={cloud}
@@ -156,7 +133,7 @@ export function JelajahTeknik() {
                             position: 'absolute',
                         }}
                     />
-                </div>
+                </div> */}
             </div>
             <div className='card-two w-[35vw] flex-row items-center justify-center hidden pr-[2rem] md:flex'>
                 <div className='h-[55vmin] w-[55vmin]'>
@@ -179,6 +156,24 @@ export function JelajahTeknik() {
                 <div className='py-8 min-h-screen overflow-hidden'>
                     <PageTitle title='Jelajah Teknik' />
                     <div className='mt-[-4rem]'>
+                        <div className='relative'>
+                            <div className='w-[45vmin] h-[25vmin] z-3 block absolute right-[2vw] top-[5vh] lg:top-[3.5vh] lg:right-[0vw] '>
+                                <img
+                                    className='w-100 h-100 z-[3] flex absolute scale-[70%] lg:scale-[78%]'
+                                    src={cloud}
+                                    alt=''
+                                />
+                            </div>
+                        </div>
+                        <div className='relative'>
+                            <div className='w-[45vmin] h-[25vmin] z-3 block absolute left-[2vw] bottom-[-190vmin] md:bottom-[-92vmin] md:left-[0vw] '>
+                                <img
+                                    className='w-100 h-100 z-[3] flex absolute scale-[70%] lg:scale-[78%]'
+                                    src={cloud}
+                                    alt=''
+                                />
+                            </div>
+                        </div>
                         <SliderContainer Children={Children} />
                     </div>
                 </div>
