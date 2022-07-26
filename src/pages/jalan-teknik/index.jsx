9aa05cda171bc661c8ebe55486eb4e608515b64c
Mufidus Sani <sanimufidus@gmail.com>
2022-07-26 10:27:25 +0700

    fix(jelajah-teknik): layout restructure

--- src/pages/jalan-teknik/index.jsx
+++ src/pages/jalan-teknik/index.jsx
@@ -18,13 +18,13 @@ export function JelajahTeknik() {
     const Children = [
         <>
             <div className='card-one w-[45vw] 768px:w-[65vw] flex-col flex justify-center px-[3rem] 768px:px-[1rem]'>
-                <h3 className='font-gill font-bold text-white text-center text-[4vmin] 1024px:text-[3.5vmin] 768px:text-[4.5vmin] my-[1.5rem]'>
+                <h3 className='font-gill font-bold text-white text-center text-[36px] 1024px:text-[3.5vmin] 768px:text-[4.5vmin] my-[1.5rem]'>
                     NGULIK TEKNIK
                 </h3>
                 <div className='items-center justify-center flex h-100 w-100 ml-auto mr-auto md:hidden'>
                     <img src={ngulik} alt='Foto Ngulik Teknik' />
                 </div>
-                <p className='font-gill font-bold text-white text-center text-[3vmin] 1024px:text-[2.5vmin] 768px:text-[3vmin] mb-[1.5rem] 768px:my-[1.5rem]'>
+                <p className='font-gill text-white text-center font-medium 768px:font-normal text-[20px] 1024px:text-[2.5vmin] 768px:text-[3vmin] mb-[1.5rem] 768px:my-[1.5rem]'>
                     Ngulik Teknik merupakan salah satu sub-kegiatan PPSMB
                     (Pelatihan Pembelajar Sukses untuk Mahasiswa Baru) KESATRIA
                     2022 yang memberikan wawasan dan pengetahuan kepada
@@ -33,9 +33,9 @@ export function JelajahTeknik() {
                     terdapat di Fakultas Teknik Universitas Gadjah Mada.
                 </p>
                 <div className='button-container justify-center items-center flex flex-row'>
-                    <div className=' text-white text-[3vmin] md:text-[3vmin] mb-[1.5rem]'>
+                    <div className=' text-white text-[3vmin] md:text-[18px] mb-[1.5rem]'>
                         <button
-                            className='font-gill font-bold bg-[#F26628] rounded-full p-[0.5rem] px-[1rem]'
+                            className='font-gill font-medium bg-[#F26628] rounded-full p-[0.5rem] px-[1.5rem]'
                             type='button'
                         >
                             <Link to='jelajah-teknik/ngulik-teknik'>
@@ -52,14 +52,14 @@ export function JelajahTeknik() {
             </div>
         </>,
         <>
-            <div className='card-one w-[45vw] 768px:w-[65vw] flex-col flex justify-center px-[3rem] 768px:px-[1rem]'>
-                <h3 className='font-gill font-bold text-white text-center text-[4vmin] 1024px:text-[3.5vmin] 768px:text-[4.5vmin] my-[1.5rem]'>
+            <div className='card-one w-[45vw] 768px:w-[65vw] flex-col flex justify-center px-[3rem] font-medium 768px:font-normal 768px:px-[1rem]'>
+                <h3 className='font-gill font-bold text-white text-center text-[36px] 1024px:text-[3.5vmin] 768px:text-[4.5vmin] my-[1.5rem]'>
                     JALAN-JALAN TEKNIK
                 </h3>
                 <div className='items-center justify-center flex h-100 w-100 mr-auto ml-auto md:hidden'>
                     <img src={JJT} alt='Foto JJT' />
                 </div>
-                <p className='font-gill font-bold text-white text-center text-[3vmin] 1024px:text-[2.5vmin] 768px:text-[3vmin] mb-[1.5rem] 768px:my-[1.5rem]'>
+                <p className='font-gill text-white text-center text-[20px] 1024px:text-[2.5vmin] 768px:text-[3vmin] mb-[1.5rem] 768px:my-[1.5rem]'>
                     Jalan-jalan Teknik menjadi salah satu kegiatan yang ada di
                     PPSMB (Pelatihan Pembelajar Sukses untuk Mahasiswa Baru)
                     KESATRIA 2022. Sebagai mahasiswa teknik, penting bagi kita
@@ -70,9 +70,9 @@ export function JelajahTeknik() {
                     aktivitas perkuliahan.
                 </p>
                 <div className='button-container justify-center items-center flex flex-row'>
-                    <div className=' text-white text-[3vmin] md:text-[3vmin] mb-[1.5rem]'>
+                    <div className=' text-white text-[3vmin] md:text-[18px] mb-[1.5rem]'>
                         <button
-                            className='font-gill font-bold bg-[#F26628] rounded-full p-[0.5rem] px-[1rem]'
+                            className='font-gill font-medium bg-[#F26628] rounded-full p-[0.5rem] px-[1.5rem]'
                             type='button'
                         >
                             <Link to='jelajah-teknik/jalan-jalan-teknik'>
